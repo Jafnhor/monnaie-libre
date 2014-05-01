32ac47f40c018a940f24b8af82c9412e1519ef8f
SirGladius <brisset.fabien@hotmail.fr>
2014-05-01 18:11:26 +0200

    Tri sur les services + changement des services.yml + changement des routes donc plus de chemins classiques + upload d'images + modification des monnaies par celle choisie par Florian
    Conflicts:
    	Symfony/src/Ml/ServiceBundle/Controller/ServiceController.php

--- Symfony/src/Ml/ServiceBundle/Entity/Sale.php
+++ Symfony/src/Ml/ServiceBundle/Entity/Sale.php
@@ -3,6 +3,7 @@
 namespace Ml\ServiceBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Sale
@@ -29,16 +30,20 @@ class Sale extends Service
     protected $type;
 	
 	/**
-     * @var string
-     *
-     * @ORM\Column(name="linkHostedPicture", type="string", length=255, nullable=true)
+     * @Assert\File(maxSize="6000000")
+	 * @ORM\Column(name="file", type="string", length=255, nullable=true)
      */
-    protected $linkHostedPicture;
+    protected $file;
+	
+	/**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    public $path;
 	
 	 public function __construct() {
         parent::__construct();
 		$this->type = "Sale";
-		$this->linkHostedPicture = NULL;
+		$this->file = NULL;
 	}
 
     /**
@@ -74,26 +79,95 @@ class Sale extends Service
         return $this->type;
     }
 
+
+	public function getAbsolutePath()
+    {
+        return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path ? null : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // le chemin absolu du répertoire où les documents uploadés doivent être sauvegardés
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // on se débarrasse de « __DIR__ » afin de ne pas avoir de problème lorsqu'on affiche
+        // le document/image dans la vue.
+        return 'uploads/documents';
+    }
+	
+	public function upload() {
+		// la propriété « file » peut être vide si le champ n'est pas requis
+		if (null === $this->file) {
+			return;
+		}
+
+		// utilisez le nom de fichier original ici mais
+		// vous devriez « l'assainir » pour au moins éviter
+		// quelconques problèmes de sécurité
+
+		// la méthode « move » prend comme arguments le répertoire cible et
+		// le nom de fichier cible où le fichier doit être déplacé
+		$this->file->move($this->getUploadRootDir(), $this->file->getClientOriginalName());
+
+		// définit la propriété « path » comme étant le nom de fichier où vous
+		// avez stocké le fichier
+		$this->path = $this->file->getClientOriginalName();
+
+		// « nettoie » la propriété « file » comme vous n'en aurez plus besoin
+		$this->file = null;
+	}
+
     /**
-     * Set linkHostedPicture
+     * Set file
      *
-     * @param string $linkHostedPicture
+     * @param string $file
      * @return Sale
      */
-    public function setLinkHostedPicture($linkHostedPicture)
+    public function setFile($file)
     {
-        $this->linkHostedPicture = $linkHostedPicture;
+        $this->file = $file;
     
         return $this;
     }
 
     /**
-     * Get linkHostedPicture
+     * Get file
      *
      * @return string 
      */
-    public function getLinkHostedPicture()
+    public function getFile()
     {
-        return $this->linkHostedPicture;
+        return $this->file;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Sale
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
     }
 }
