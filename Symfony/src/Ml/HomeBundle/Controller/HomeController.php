efa0fa90559073b6c426531bcd3622e0702d1eb8
SirGladius <brisset.fabien@hotmail.fr>
2014-05-15 23:19:05 +0200

    Premiere version de 'developpeurs', les photos sont a modifier par vos soins en respectant tout de meme des tailles correctes

--- Symfony/src/Ml/HomeBundle/Controller/HomeController.php
+++ Symfony/src/Ml/HomeBundle/Controller/HomeController.php
@@ -40,4 +40,9 @@ class HomeController extends Controller
 			}
 		}
     }
+	
+	public function developersAction()
+    {	
+		return $this->render('MlHomeBundle:Home:developers.html.twig');
+    }
 }
