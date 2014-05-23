811a09847b6b37fec019a3aee57c0ef5aad01af5
Plazas Adrien <adrien.plazas@etud.univ-montp2.fr>
2014-05-23 09:07:58 +0200

    Suppression des indexs de la documentation (doublon avec la table des matières)

--- rapport/extracteur-refman.py
+++ rapport/extracteur-refman.py
@@ -7,20 +7,30 @@ refman = open('latex/refman.tex','r')
 lines = refman.readlines()
 
 inputline = re.compile(".*input{(.*)")
-chapterline = re.compile(".*chapter{(.*)")
+indexchapterline = re.compile(".*chapter{(Index .*)")
+docchapterline = re.compile(".*chapter{(Documentation .*)")
 
-found = False
+accepted_chapter = False
 
-sortie.write("\\chapter{Manuel de référence}\n\n")
+#sortie.write("\\chapter{Manuel de référence}\n\n")
 
 for line in lines:
+	# Ne pas accepter les chapitres d'index (doublon avec la table des matières)
+	result = indexchapterline.match(line)
+	if (result):
+		accepted_chapter = False
+	
+	# Accepter les chapitres de documentation
+	result = docchapterline.match(line)
+	if (result):
+		accepted_chapter = True
+	
+	# Transformer les input en import (définit le dossier pour les inclusions récursives)
 	result = inputline.match(line)
 	if (result):
 		line = "\\import{latex/}{" + result.group(1) + "\n"
-		sortie.write(line)
-	result = chapterline.match(line)
-	if (result):
-		line = "\\section{" + result.group(1) + "\n"
+	
+	if (accepted_chapter):
 		sortie.write(line)
 
 refman.close()
